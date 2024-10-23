27378bced83efb80e55d6ebe175729b21faf944f
Javi <in2dm3-v@elorrieta.int>
2024-10-23 18:01:36 +0200

    inicio de sharedPreferenced

--- app/src/main/java/com/example/gym_app/LoginActivity.kt
+++ app/src/main/java/com/example/gym_app/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.gym_app
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +15,10 @@ class LoginActivity : AppCompatActivity() {
 
         val registerButton: Button = findViewById(R.id.registerButton)
         val goingToMenu: Button = findViewById(R.id.menu)
+        val rememberMe: CheckBox = findViewById(R.id.rememberMe)
+        val userText: EditText = findViewById(R.id.userText)
+        val passwordText: EditText = findViewById(R.id.passwordText)
+        lateinit var sharedP: SharedPreferences
 
         registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
@@ -20,6 +27,17 @@ class LoginActivity : AppCompatActivity() {
         goingToMenu.setOnClickListener {
             val intent = Intent(this, WorkoutsActivity::class.java)
             startActivity(intent)
+            if(rememberMe.isActivated){
+                val editor: SharedPreferences.Editor = sharedP.edit()
+                editor.putString("user", userText.text.toString())
+                editor.putString("password", passwordText.text.toString())
+                editor.apply()
+            } else {
+                val editor: SharedPreferences.Editor = sharedP.edit()
+                editor.remove("user")
+                editor.remove("password")
+                editor.apply()
+            }
         }
     }
 }
