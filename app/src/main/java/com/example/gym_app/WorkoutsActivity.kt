eb7b29b9d5e185bec545642f71baf715b775d945
corderaa <ugaitzcordero1@gmail.com>
2024-10-25 18:42:19 +0200

    Añadido funcionamiento login y workout lis

--- app/src/main/java/com/example/gym_app/WorkoutsActivity.kt
+++ app/src/main/java/com/example/gym_app/WorkoutsActivity.kt
@@ -3,7 +3,10 @@ package com.example.gym_app
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gym_app.model.WorkOutItem
+import com.example.gym_app.model.WorkoutItemArrayAdapter
 
 class WorkoutsActivity : AppCompatActivity() {
 
@@ -11,10 +14,21 @@ class WorkoutsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.workouts)
 
+        val myList = findViewById<ListView>(R.id.listView)
+        val workoutItem = mutableListOf<WorkOutItem>()
+
+        // DumMy items
+        workoutItem.add(WorkOutItem("Push-ups", 10, "20 minutes", "14 minutes", "29 Enero", "50%"))
+        workoutItem.add(WorkOutItem("Push-ups", 10, "20 minutes", "14 minutes", "29 Enero", "50%"))
+
+
+
+        val adapter = WorkoutItemArrayAdapter(this, R.layout.workout_item, workoutItem)
+        myList.adapter = adapter
+
         val returnButton: Button = findViewById(R.id.returnButton)
         val profileButton: Button = findViewById(R.id.profileButton)
         val coachButton: Button = findViewById(R.id.coachButton)
-        val playButton: Button = findViewById(R.id.playButton)
 
         returnButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -29,9 +43,12 @@ class WorkoutsActivity : AppCompatActivity() {
             val intent = Intent(this, CoachActivity::class.java)
             startActivity(intent)
         }
-        playButton.setOnClickListener {
-            val intent = Intent(this, PlayActivity::class.java)
-            startActivity(intent)
-        }
+
+
+
+       // playButton.setOnClickListener {
+       //     val intent = Intent(this, PlayActivity::class.java)
+        //    startActivity(intent)
+        //}
     }
 }
